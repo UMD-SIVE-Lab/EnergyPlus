4c7f98a581952578a99323f3097c32c78ea5ee06
Mark Adams <adamsmb@ornl.gov>
2016-03-14 09:51:10 -0400

    Fix merge conflicts
    # Conflicts:
    #	src/EnergyPlus/Furnaces.hh
    #	src/EnergyPlus/VariableSpeedCoils.hh
    #	src/EnergyPlus/WaterThermalTanks.hh

--- src/EnergyPlus/VariableSpeedCoils.hh
+++ src/EnergyPlus/VariableSpeedCoils.hh
@@ -748,45 +748,20 @@ namespace VariableSpeedCoils {
 		int const CyclingScheme // Continuous fan OR cycling compressor
 	);
 
-<<<<<<< HEAD
-	void SetAirNodes(std::string const & CoilName, // must match coil names for the coil type
+	void SetAirNodes(
+		std::string const & CoilName, // must match coil names for the coil type
 		bool & ErrorsFound, // set to true if problem
 		int const InNode,
 		int const OutNode
-		); 
+	); 
 
 	void SetWaterNodes(
 		std::string const & CoilName, // must match coil names for the coil type
 		bool & ErrorsFound, // set to true if problem
 		int const InNode,
 		int const OutNode
-		); 
+	); 
 
-	//     NOTICE
-
-	//     Copyright (c) 1996-2015 The Board of Trustees of the University of Illinois
-	//     and The Regents of the University of California through Ernest Orlando Lawrence
-	//     Berkeley National Laboratory.  All rights reserved.
-
-	//     Portions of the EnergyPlus software package have been developed and copyrighted
-	//     by other individuals, companies and institutions.  These portions have been
-	//     incorporated into the EnergyPlus software package under license.   For a complete
-	//     list of contributors, see "Notice" located in main.cc.
-
-	//     NOTICE: The U.S. Government is granted for itself and others acting on its
-	//     behalf a paid-up, nonexclusive, irrevocable, worldwide license in this data to
-	//     reproduce, prepare derivative works, and perform publicly and display publicly.
-	//     Beginning five (5) years after permission to assert copyright is granted,
-	//     subject to two possible five year renewals, the U.S. Government is granted for
-	//     itself and others acting on its behalf a paid-up, non-exclusive, irrevocable
-	//     worldwide license in this data to reproduce, prepare derivative works,
-	//     distribute copies to the public, perform publicly and display publicly, and to
-	//     permit others to do so.
-
-	//     TRADEMARKS: EnergyPlus is a trademark of the US Department of Energy.
-
-=======
->>>>>>> develop
 } // VariableSpeedCoils
 
 } // EnergyPlus
